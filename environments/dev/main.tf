d44f3e058ec8d4f10eaf2a6d438f68c2e614beca
Wasim30596 <wasim3096k@gmail.com>
2026-04-14 18:34:02 +0530

    updated ami instance

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -9,7 +9,7 @@ module "vpc" {
 module "ec2" {
   source               = "../../modules/ec2"
   ec2_ami              = "ami-05d2d839d4f73aafb"
-  ec2_instance_type    = "t2.micro"  
+  ec2_instance_type    = "t3.micro"  
   ec2_instance_count   = 2
   env                  = "dev"
   ec2_subnet_id        = module.vpc.subnet_id
