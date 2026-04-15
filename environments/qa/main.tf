cc078dcb3fd9cc64e3de301e151c6561060b8405
Wasim30596 <wasim3096k@gmail.com>
2026-04-15 06:01:54 +0530

    instance count changed

--- environments/qa/main.tf
+++ environments/qa/main.tf
@@ -2,7 +2,7 @@ module "ec2" {
   source               = "../../modules/ec2"
   ec2_ami              = "ami-05d2d839d4f73aafb"
   ec2_instance_type    = "t3.micro"  
-  ec2_instance_count   = 2
+  ec2_instance_count   = 1
   env                  = "dev"
   
 }
