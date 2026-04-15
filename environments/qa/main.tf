433af4f933e4ac3aeeee1ca2e5f0b5d82b541acc
Wasim30596 <wasim3096k@gmail.com>
2026-04-15 06:06:48 +0530

    changed env variable in main.tf

--- environments/qa/main.tf
+++ environments/qa/main.tf
@@ -3,6 +3,6 @@ module "ec2" {
   ec2_ami              = "ami-05d2d839d4f73aafb"
   ec2_instance_type    = "t3.micro"  
   ec2_instance_count   = 1
-  env                  = "dev"
+  env                  = "qa"
   
 }
